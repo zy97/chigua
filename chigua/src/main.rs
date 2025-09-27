9f24afb5ab965f8fb938979d1f0c995dc18d1e00
bomky <bom_ky@Outlook.com>
2025-09-27 17:17:39 +0800

    release: 0.6.0

--- chigua/src/main.rs
+++ chigua/src/main.rs
@@ -9,9 +9,7 @@ use serde::Serialize;
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new()
-        .route("/", get(handler))
-        .route("/chigua/{name}", get(get_chigua_url));
+    let app = Router::new().route("/chigua/{name}", get(get_chigua_url));
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -21,9 +19,6 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World </h1>")
-}
 async fn get_chigua_url(extract::Path(name): extract::Path<String>) -> impl IntoResponse {
     // Load the JSON file and parse it
     let json_file =
