e50a41ffa78e9be1e1edefefa269ac9690b7b046
bomky <bom_ky@Outlook.com>
2025-09-27 17:31:12 +0800

    绑定所有ip

--- chigua/src/main.rs
+++ chigua/src/main.rs
@@ -1,9 +1,4 @@
-use axum::{
-    Json, Router, extract,
-    http::StatusCode,
-    response::{Html, IntoResponse},
-    routing::get,
-};
+use axum::{Json, Router, extract, http::StatusCode, response::IntoResponse, routing::get};
 use serde::Serialize;
 
 #[tokio::main]
@@ -12,9 +7,7 @@ async fn main() {
     let app = Router::new().route("/chigua/{name}", get(get_chigua_url));
 
     // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
