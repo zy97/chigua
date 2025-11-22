4e98102d145acd3dd79eb2f5e4bc532f8dab3a5f
bomky <bom_ky@Outlook.com>
2025-11-22 14:02:23 +0800

    黑料更新成黑料网

--- chigua/src/main.rs
+++ chigua/src/main.rs
@@ -7,7 +7,7 @@ async fn main() {
     let app = Router::new().route("/chigua/{name}", get(get_chigua_url));
 
     // run it
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:18181").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
